c35aa2da93ddec56add2efbc8d32a2365b333332
Elvin Baghele <join.eb@gmail.com>
2020-09-10 13:43:19 +0530

    added staging env configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendURL: 'http://upayfinancialapprovalsystem-env.sfgn9ic5zm.us-east-2.elasticbeanstalk.com/'
+  backendURL: 'http://UpayFinancialApprovalSystem-env.sfgn9ic5zm.us-east-2.elasticbeanstalk.com/'
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    backendURL: 'http://upayfinancialapprovalsystem-dev.us-east-2.elasticbeanstalk.com/'
+  };
+  
