3ea533799b06db75db5c0ef9cb1d34a84c06f2f2
Sathvik <sathvikp80@gmail.com>
2025-03-02 17:57:36 +0530

    showSettings logic fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     const userZone = this.authService.getUserZone();
     const reviewAdmin = this.authService.getReviewUserZone();
-    if (userZone === 'admin' && reviewAdmin ==='false') {
+    if (userZone === 'admin') {
 
       this.showSettings = true;
     }
